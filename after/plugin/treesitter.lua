e5b0d58a16cdf314f63543ba84ac769c7563c8a9
scarpma <scarpma@gmail.com>
2023-04-25 12:08:30 +0200

    remove julia lang

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "cpp", "lua", "python", "julia", "fortran"},
+  ensure_installed = { "c", "cpp", "lua", "python", "fortran"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
